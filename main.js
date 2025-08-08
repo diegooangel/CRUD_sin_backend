15f92f2fab7a01aac2fc37c7260b9a411de9732d
diegooangel <diego09.11.2000@gmail.com>
2025-08-08 10:21:26 -0300

    terminamos el CRUD, ultima funcion creada el poder editar

--- main.js
+++ main.js
@@ -8,24 +8,37 @@ const div = document.getElementById('div');
 
 
 let notas = [];
+let editando = false;
+let idEditando = null;
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 });
 
 button.addEventListener('click', () => {
-    console.log(input.value, text.value);
     const titulo = input.value;
     const tarea = text.value;
-    if (titulo.trim() == "" || tarea.trim() == "") {
-        alert("no se puede agregar titulos o tareas vacias");
+    if (editando) {
+        notas = notas.map(nota => {
+            if (nota.id == idEditando) {
+                return {...nota, titulo: titulo, tarea: tarea}
+            } else {
+                return nota;                
+            }
+        });
+        editando = false;
+        idEditando = null;
     } else {
-
-        notas.push({id: Date.now(),titulo: titulo, tarea: tarea}); //date.now() lo use para darle un id unico.
-        mostrarNotas();
-        input.value = "";
-        text.value = "";
+        if (titulo.trim() == "" || tarea.trim() == "") {
+            alert("no se puede agregar titulos o tareas vacias");
+        } else {
+            notas.push({id: Date.now(),titulo: titulo, tarea: tarea}); //date.now() lo use para darle un id unico.       
     }
+    }
+    button.textContent = "Enviar"
+    mostrarNotas();
+    input.value = "";
+    text.value = "";
 });
 
 
@@ -39,14 +52,27 @@ function mostrarNotas() {
             <p>${element.tarea}</p>
         </div> `
         ul.appendChild(li);
+//aca creamos el boton eliminar
         const botonEliminar = document.createElement('button');
         botonEliminar.setAttribute('data-id', element.id);
-        botonEliminar.textContent = "edit"
+        botonEliminar.textContent = "delete"
         li.appendChild(botonEliminar);
         botonEliminar.addEventListener('click', (e) => {
             console.log(e.target.dataset.id); //asi accedo al numero de data-id del boton.
             const data_id = parseInt(e.target.dataset.id); //pase de string a entero el data_id
             eliminar(data_id);
+        });
+
+//aca creamos el boton edit
+        const botonEdit = document.createElement('button');
+        botonEdit.textContent = "edit";
+        botonEdit.setAttribute('data-id', element.id);
+        li.appendChild(botonEdit);
+        botonEdit.addEventListener('click', (e) => {
+            console.log(`${e.target.dataset.id} ESTO ES DEL BOTON EDIT`);
+            const data_id = parseInt(e.target.dataset.id);
+            editando = true;
+            edit(data_id);
         });
     });
 }
@@ -55,6 +81,12 @@ function eliminar(params) {
     const nuevaNotas = notas.filter(nota => nota.id !== params);  
     notas = nuevaNotas;
     mostrarNotas();
+}
+
+function edit(params) {
+    idEditando = params;
+    button.textContent = "Modificar tarea"
+    console.log(notas);
 }
 /*
 filter() devuelve un nuevo array (osea nuevaNotas) con la condicion indicada, en este caso que no sea igual al parametro, asi me deja todos los
